afe02452857207a63e8e48400680944b4b5c460f
Daniel Felipe Venegas <danyvesar97@gmail.com>
2025-12-12 20:36:57 -0500

    Creado el proyecto general

---
+++ sistemapagosback/src/main/java/com/lta/sistemapagos/SistemapagosbackApplication.java
@@ -0,0 +1,13 @@
+package com.lta.sistemapagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemapagosbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemapagosbackApplication.class, args);
+    }
+
+}
---
+++ sistemapagosback/src/test/java/com/lta/sistemapagos/SistemapagosbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lta.sistemapagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemapagosbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
