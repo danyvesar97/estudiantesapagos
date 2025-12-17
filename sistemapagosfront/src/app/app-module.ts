7019a8fa453451d829eb07a9af53fbb88b24f002
Daniel Felipe Venegas <danyvesar97@gmail.com>
2025-12-17 13:28:39 -0500

    Creado el toolbar básico

--- sistemapagosfront/src/app/app-module.ts
+++ sistemapagosfront/src/app/app-module.ts
@@ -1,16 +1,26 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {NgModule, provideBrowserGlobalErrorListeners} from '@angular/core';
+import {BrowserModule, provideClientHydration, withEventReplay} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing-module';
-import { App } from './app';
+import {AppRoutingModule} from './app-routing-module';
+import {App} from './app';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {AdminTemplate} from './admin-template/admin-template';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    AdminTemplate,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
@@ -18,4 +28,5 @@ import { App } from './app';
   ],
   bootstrap: [App]
 })
-export class AppModule { }
+export class AppModule {
+}
